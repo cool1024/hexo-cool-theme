3ea0a8bf9add7f63f50af31a8b34cdaf375bd97a
cool1024 <xiaojiannangkang@126.com>
2019-11-14 14:53:44 +0800

    update theme

--- themes/cool1024/scripts/tag.js
+++ themes/cool1024/scripts/tag.js
@@ -6,3 +6,13 @@ hexo.extend.helper.register('tag_color', function () {
 hexo.extend.helper.register('image_hold', function (url) {
   return `<div class="w-100 h-100" style="background-image:url(${url});background-position:center;background-repeat:no-repeat;background-size:cover;"></div>`
 });
+
+hexo.extend.helper.register('tags_find', function (tags, name) {
+  var oc = [];
+  tags.each(function (c) {
+      if (c.name === name) {
+          oc = c;
+      }
+  });
+  return oc;
+});
